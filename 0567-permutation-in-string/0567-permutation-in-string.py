39a9b33f27398387e01b79703cf2302b7194d38f
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2022-10-13 13:30:10 -0700

    Time: 4223 ms (13.12%), Space: 14.1 MB (6.76%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,12 +1,12 @@
 from itertools import permutations
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        c = Counter(s1)
+        s1 = "".join(sorted(s1))
         
         for i in range(len(s2)-len(s1)+1):
-            c2 = Counter(s2[i:i+len(s1)])
-            if c2 == c:
+            if "".join(sorted(s2[i:i+len(s1)])) == s1:
                 return True
         return False
+        
         
         
