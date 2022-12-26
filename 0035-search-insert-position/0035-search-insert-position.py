1f9fa1248d929442f917607f8425a4604e9d77ba
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2022-12-26 11:42:32 -0700

    Time: 46 ms (97.84%), Space: 14.6 MB (99.00%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -4,21 +4,71 @@ class Solution:
         right = len(nums)-1
         
         while left < right:
-            mid = left+(right-left)//2
-
+            mid = left + (right-left) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid]<target:
-                left = mid
-            else:
+            elif nums[mid] > target:
                 right = mid
-            if left+1 == right:
+            else:
+                left = mid
+            if left + 1 == right:
                 break
-        
-        if target <  nums[left] or target == nums[left]:
+        if target < nums[left] or target == nums[left]:
             mid = left
-        elif target>nums[right]:
-            mid = right + 1
+        elif target > nums[right]:
+            mid = right+1
         else:
             mid = right
         return mid
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         left = 0
+#         right = len(nums)-1
+        
+#         while left < right:
+#             mid = left+(right-left)//2
+
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid]<target:
+#                 left = mid
+#             else:
+#                 right = mid
+#             if left+1 == right:
+#                 break
+        
+#         if target <  nums[left] or target == nums[left]:
+#             mid = left
+#         elif target>nums[right]:
+#             mid = right + 1
+#         else:
+#             mid = right
+#         return mid
