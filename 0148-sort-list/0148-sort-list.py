455cb97fc7d5881aa04e234f21d32928637df86b
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-01-06 14:19:30 -0700

    Time: 892 ms (52.64%), Space: 86.6 MB (5.43%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,38 +5,42 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
         if head == None or head.next == None:
             return head
-        mid = self.mid(head)
+        mid = self.getMid(head)
+        #print(mid)
         mid1 = mid.next
         mid.next = None
-        
         l = self.sortList(head)
         r = self.sortList(mid1)
-        
-        res = self.merge(l,r)
-        
+        res = self.merge(l, r)
         return res
-    def merge(self,l,r):
+        
+    def merge(self, l ,r):
         if not l:
             return r
         if not r:
             return l
-        res = None
         if l.val < r.val:
             res = l
             res.next = self.merge(l.next,r)
         else:
             res = r
-            res.next = self.merge(l,r.next)
+            res.next = self.merge(l, r.next)
         return res
-    def mid(self,head):
+    
+    
+    def getMid(self,head):
         slow = head
         fast = head
         
-        while fast.next and fast.next.next:
+        while fast and fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
+        
         return slow
+        
+
             
         
