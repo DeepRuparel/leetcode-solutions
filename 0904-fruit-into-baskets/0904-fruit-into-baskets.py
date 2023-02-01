bebca944653759c09bf38d486808608553142d84
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-02-01 10:20:42 -0700

    Time: 889 ms (86.48%), Space: 20.2 MB (51.40%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,13 +1,23 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         basket = {}
-        left = 0
+        left = 0 
         for right, fruit in enumerate(fruits):
             basket[fruit] = basket.get(fruit, 0) + 1
             if len(basket) > 2:
                 basket[fruits[left]] -= 1
-            
                 if basket[fruits[left]] == 0:
                     del basket[fruits[left]]
-                left += 1
+                left+=1
         return right-left+1
+#         basket = {}
+#         left = 0
+#         for right, fruit in enumerate(fruits):
+#             basket[fruit] = basket.get(fruit, 0) + 1
+#             if len(basket) > 2:
+#                 basket[fruits[left]] -= 1
+            
+#                 if basket[fruits[left]] == 0:
+#                     del basket[fruits[left]]
+#                 left += 1
+#         return right-left+1
