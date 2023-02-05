f8dc0568bca42f0a967042413627a3401b596bf7
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-02-05 09:41:39 -0700

    Time: 20 ms (45.24%), Space: 47.2 MB (80.80%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,4 +1,11 @@
 class Solution {
+    /*
+    Approach: 
+    Create a maxHeap and start adding elements
+    If the size if greater than k we will remove the top most element and at the end the kth smallest element will be on top
+    Time : O(m*n*logk) m is the number of rows, n is the no of columns
+    Space : O(k) since only ke elments are kept in the heap.
+    */
     public int kthSmallest(int[][] matrix, int k) {
         int m = matrix.length, n = matrix[0].length;
         //creating a maxheap
