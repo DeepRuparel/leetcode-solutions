ec52d60ca4e4c3b64e80010128ff70db98b13384
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-02-19 09:44:35 -0700

    Time: 25 ms (33.56%), Space: 13.6 MB (89.27%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -1,31 +1,40 @@
+'''
+since we want all the leaves at the same level we have to bfs
+also add a varibale when even doesnpt reverse the list andd when odd reverses the list and appends it to ans
+'''
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        #base condition for emptty root
         if not root:
             return []
-        
-        queue = deque([root])
-        pattern = 0
+        queue = deque()
+        queue.append(root)
         ans = []
+        isEven = 0
         while queue:
             temp = []
             for i in range(len(queue)):
                 node = queue.popleft()
+                temp.append(node.val)
+                #check if right and left and append them to queue
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-                temp.append(node.val)
-            if pattern % 2 == 1:
+            if isEven % 2 == 1:
                 temp = temp[::-1]
-                pattern = 0
-            else:
-                pattern = 1
+            isEven += 1
             ans.append(temp)
         return ans
             
+        
