2116f25bfd227e204bbfdbff402a51d420e365ca
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-02-23 15:40:39 -0700

    Time: 29 ms (40.16%), Space: 13.5 MB (39.21%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,44 +1,94 @@
+'''
+get the length of the list 
+subtract k from it 
+and go on traversing the list until n - k > 0
+# note observed that k > n so do k = k % n
+when the loop exits 
+store the next node in temp and temphead
+make the nodes'next as none
+go until the end of temp make it's none point to head
+make head = temp
+
+dry run:
+1 -> 2 -> 3 -> 4 -> 5
+
+n = 5
+k % 5 = 3 % 5 = 3
+while k > 0
+1 -> 2 -> 3 -> 4 -> 5
+        here
+        
+temp = here.next
+temphead = temp
+make here.next = None
+1 -> 2 -> 3 -> None. 4 -> 5
+                     temp
+                     tempHead
+while temp.next != None
+increase temp
+
+after loop 
+1 -> 2 -> 3 -> None. 4 ->       5 ->None
+                    tempHead    temp
+temp.next = Head
+
+4 -> 5 -> 1 -> 2 -> 3 -> None
+make tempHead = head
+4 -> 5 -> 1 -> 2 -> 3 -> None
+Head
+
+This didn't work need to refine it
+
+1 -> 2 -> 3 -> 4 -> 5
+^ - -  -  -  -  -  -|
+k = k % n
+k = 2
+
+curr = head
+
+till n - k - 1 = 6 - 2 - 1 = 3
+curr = curr.next
+
+1 -> 2 -> 3 -> 4 -> 5
+^ - - - - - - - - - |
+          curr
+curr.next = newhead
+curr.next = none
+
+
+
+temp = Head
+go 
+'''
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
         if not head:
             return head
-        length = 1
-        
+        n = 1
         curr = head
         while curr.next:
-            length+=1
             curr = curr.next
-        
+            n += 1
         curr.next = head
-        
+        k = k % n
         curr = head
-        k %= length
-        for i in range(length-k-1):
+        for i in range(n - k - 1):
             curr = curr.next
+        
         newHead = curr.next
         curr.next = None
         return newHead
         
-#         length = 1
-#         curr = head
-#         while curr.next:
-#             length+=1
-#             curr = curr.next
-#         #print(curr.val)
-#         curr.next = head
-#         curr = head
-        
-#         k %= length
-#         for i in range(length-k-1):
-#             curr = curr.next
-#         #print(curr.val)
-#         newhead = curr.next
-#         curr.next = None
-#         return newhead
+       
             
         
