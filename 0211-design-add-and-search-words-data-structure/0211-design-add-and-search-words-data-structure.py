d1d673f801f340156c2b8fefdbfbb86aab0a4929
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-03-18 18:00:42 -0700

    Time: 7376 ms (86.42%), Space: 56.3 MB (82.07%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -4,26 +4,24 @@ class WordDictionary:
         self.d = {}
 
     def addWord(self, word: str) -> None:
-        curr = self.d
-        for l in word:
-            if l not in curr:
-                curr[l] = {}
-            curr = curr[l]
-        curr['#'] = {}
-        
+        d = self.d
+        for i in word:
+            if i not in d:
+                d[i] = {}
+            d = d[i]
+        d['#'] = {}
+                
 
     def search(self, word: str) -> bool:
-        def _search(word, trie):
+        def _search(word, d):
             if not word:
-                return '#' in trie
+                return '#' in d
             if word[0] == '.':
-                return any(_search(word[1:],v) for v in trie.values())
-            if word[0] not in trie:
+                return any(_search(word[1:], v) for v in d.values())
+            if word[0] not in d:
                 return False
-            return _search(word[1:],trie[word[0]])
-        return _search(word,self.d)
-                    
-        
+            return _search(word[1:], d[word[0]])
+        return _search(word, self.d)
 
 
 # Your WordDictionary object will be instantiated and called as such:
