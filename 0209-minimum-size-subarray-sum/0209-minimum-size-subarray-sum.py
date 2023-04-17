4efbc800d1a85576e35bfe40a5442a99c7c5f2d9
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-04-17 09:22:37 -0700

    Time: 253 ms (77.06%), Space: 27.1 MB (82.83%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,23 +1,34 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        i = 0 
-        j = 0 
-        sum1 = 0 
+        left = 0
+        right = 0
+        
+        curr_sum = 0
         ans = float("inf")
-        while j < len(nums):
-            sum1 += nums[j]
-            if sum1 < target:
-                j += 1
-            elif sum1 >= target:
-                while sum1 >= target:
-
-                    ans = min(ans, j-i+1)
-
-                    sum1 -= nums[i]
-
-                    i += 1
-                j += 1 
-        if ans == float("inf"):
+        
+        # traverse the given nums
+        
+        while right < len(nums):
+            #adding the current number to sum
+            curr_sum += nums[right]
+            
+            #check if curr sum if less than target which means I can add more things to it
+            
+            if curr_sum < target:
+                right += 1
+            
+            else:
+                # this means I need to keep removing numbers from the left until the sum is less than target
+                while curr_sum >= target:
+                    #geeting the current size of the numbers which lead to curr_sum
+                    ans = min(ans, right - left + 1)
+                    # removing the leftmost element
+                    curr_sum -= nums[left]
+                    #incrementing left
+                    left += 1
+                
+                right += 1
+        if ans == float('inf'):
             return 0
         else:
             return ans
