bb9ce07ae81c123da45febdbe244c090f8755c81
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-05-05 15:55:47 -0700

    Time: 42 ms (12.30%), Space: 16.2 MB (8.20%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,3 +1,34 @@
+'''
+do a dfs approach maybe 
+and do like a swap using a third variable temp
+    4
+   / \
+  2.  7
+ / \. / \
+1  3 6   9
+
+root = 4
+temp = root.left
+temp =  2
+       / \
+       1   3
+root.left = root.right
+root.left =   7
+            /   \
+          9      6
+root.right = temp
+root.right =  2
+             / \
+            1   3
+tree now is 
+
+        4
+      /   \
+      7    2
+    /   \  / \
+  9      6 1   3
+ do the same for all nodes
+'''
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
