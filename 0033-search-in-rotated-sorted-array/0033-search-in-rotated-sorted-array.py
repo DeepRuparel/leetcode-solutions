fd67aa5e2953ec19d348eacbd7cb1f77dd856af5
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-05-06 16:54:48 -0700

    Time: 52 ms (14.75%), Space: 16.7 MB (9.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,35 +1,36 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def findpivot(nums,left,right):
+        def findpivot(nums, left, right):
             if left > right:
                 return -1
             if left == right:
                 return left
-            mid = (left+right)//2
-            if mid < right and nums[mid]>nums[mid+1]:
-                return mid+1
-            elif mid > left and nums[mid] < nums[mid-1]:
-                return mid-1
-            elif nums[left] > nums[mid]:
-                return findpivot(nums,left,mid-1)
-            return findpivot(nums,mid+1,right)
-        def binarysearch(nums,left,right,target):
+            mid = left + (right- left)// 2
+            
+            if mid < right and nums[mid] > nums[mid + 1]:
+                return mid + 1
+            if mid > left and nums[mid] < nums[mid - 1]:
+                return mid - 1
+            if nums[left] > nums[mid]:
+                return findpivot(nums, left, mid - 1)
+            return findpivot(nums, mid +1, right)
+        
+        def bsearch(nums, left, right, target):
             while left <= right:
-                mid = (left+right)//2
-                if nums[mid]==target:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
                     return mid
-                elif nums[mid] < target:
-                    left = mid+1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    right = mid-1
+                    left = mid + 1
             return -1
+        pivot = findpivot(nums, 0, len(nums) - 1)
         
-        pivot = findpivot(nums,0,len(nums)-1)
         if pivot == -1:
-            return binarysearch(nums,0,len(nums)-1,target)
-        if nums[pivot] == target:
+            return bsearch(nums, 0, len(nums) - 1, target)
+        elif nums[pivot] == target:
             return pivot
-        if nums[0] <= target:
-            return binarysearch(nums,0,pivot-1,target)
-        return binarysearch(nums,pivot+1,len(nums)-1,target)
-        
+        elif nums[0] <= target:
+            return bsearch(nums, 0, pivot - 1, target)
+        return bsearch(nums, pivot + 1, len(nums) -1 , target)
