522cee64b6c4879dbad4a95392455ccfe12c95b1
Deep Ruparel <64136286+DeepRuparel@users.noreply.github.com>
2023-05-06 17:08:48 -0700

    Time: 102 ms (28.98%), Space: 16.7 MB (5.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,52 +1,32 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        #using the kth largest element algorithm
-        l = len(nums1) + len(nums2)
-        if l % 2 == 1:
-            return self.kth(nums1, nums2, l//2)
-        else:
-            return (self.kth(nums1, nums2, l//2) + self.kth(nums1, nums2, l//2 -1))/2
-    
-    def kth(self, nums1, nums2, k):
-        if not nums1:
-            return nums2[k]
-        if not nums2:
-            return nums1[k]
-        
-        index1 = len(nums1)//2
-        index2 = len(nums2)//2
-        m1 = nums1[index1]
-        m2 = nums2[index2]
-        
-        # if k is greater then sums of indexs
-        if index1 + index2 < k:
-            # check if median of nums1 > median2
-            #if yes then no need to include eleemnts till mb in nums2
-            if m1 > m2:
-                return self.kth(nums1, nums2[index2 +1:], k - index2 - 1)
-            else:
-                return self.kth(nums1[index1 + 1:], nums2, k - index1 -1)
-        else:
-            #if m1 > m2 it only includes elements till m1 in nums1
-            if m1 > m2:
-                return self.kth(nums1[:index1], nums2, k)
-            else:
-                return self.kth(nums1, nums2[:index2], k)
+        def kth(nums1, nums2, k):
+            if not nums1:
+                return nums2[k]
+            if not nums2:
+                return nums1[k]
             
+            ia = len(nums1)//2
+            ib = len(nums2) // 2
+            
+            ma = nums1[ia]
+            mb = nums2[ib]
+            
+            if ia + ib < k:
+                if ma > mb:
+                    return kth(nums1, nums2[ib+1 :], k -ib - 1)
+                else:
+                    return kth(nums1[ia + 1:], nums2, k - ia - 1)
+            else:
+                if ma > mb:
+                    return kth(nums1[:ia], nums2, k)
+                else:
+                    return kth(nums1, nums2[:ib], k)
+        l = len(nums1) + len(nums2)
         
+        if l % 2 == 1:
+            return kth(nums1, nums2, l//2)
+        else:
+            return ((kth(nums1, nums2, l//2) + kth(nums1, nums2, l//2 - 1)) / 2)
         
-        
-        
-        
-        #Brute force
-#         nums = nums1+nums2
-#         nums = sorted(nums)
-        
-#         if len(nums)%2 != 0:
-#             return float(nums[(len(nums)//2)])
-#         else:
-#             if len(nums)%2 == 0:
-#                 mid = len(nums)//2
-#                 ans = nums[mid]+nums[mid-1]
-#                 return float(ans/2)
         
